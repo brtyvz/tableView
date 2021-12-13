3cfd1cc9bb24957cc643ba9c811972048e4cb687
brtyvz <beratttyavuzz@gmail.com>
2021-12-13 15:23:10 +0300

    commit

--- tableView/ViewController.swift
+++ tableView/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var ulkeler : [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ulkeler = ["peru","japonya","iran","hollanda","norveç"]
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    // tableViewdeki sütun sayısını belirlemek için
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    // tableViewdeki başlıkları belirlemek için  bu fonskiyonu kullandık
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ulkeler.count
+    }
+    //tableView satırlarına dizideki ülkeleri yazdırdık ve tableview döndüren bir fonksiyon
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hucreadi", for:indexPath)
+        cell.textLabel?.text = ulkeler[indexPath.row]
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(ulkeler[indexPath.row])
+    }
 }
 
